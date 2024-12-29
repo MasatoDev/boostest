1221f4f1061469888c2e4b628030ababb6299de8
MasatoDev <masajobsenyo@gmail.com>
2024-12-29 18:47:30 +0900

    feat: update ignore name

--- crates/boostest/src/boostest_utils/ast_utils.rs
+++ crates/boostest/src/boostest_utils/ast_utils.rs
@@ -4,11 +4,46 @@ use oxc::{
     span::{Atom, Span},
 };
 
-pub fn ignore_ref_name(atom: &Atom) -> bool {
-    let name = atom.to_string();
-    name == "Array" || name == "Function" || name == "true" || name == "false" || name == "Promise"
+pub fn ignore_name(name: &str) -> bool {
+    let built_in = [
+        "true", "false", "Function", "Array",
+        "Promise",
+        // "Date",
+        // "Set",
+        // "Map",
+        // "Object",
+        // "String",
+        // "Number",
+        // "Boolean",
+        // "Symbol",
+        // "RegExp",
+        // "Error",
+        // "ArrayBuffer",
+        // "DataView",
+        // "Int8Array",
+        // "Uint8Array",
+        // "Uint8ClampedArray",
+        // "Int16Array",
+        // "Uint16Array",
+        // "Int32Array",
+        // "Uint32Array",
+        // "Float32Array",
+        // "Float64Array",
+        // "BigInt64Array",
+        // "BigUint64Array",
+        // "IterableIterator",
+        // "SharedArrayBuffer",
+        // "Atomics",
+        // "WebAssembly",
+    ];
+
+    built_in.contains(&name)
 }
 
+pub fn ignore_ref_name(atom: &Atom) -> bool {
+    let name = atom.to_string();
+    ignore_name(&name)
+}
 // common functions
 pub fn is_array_type<'a>(ts_type_ref: &allocator::Box<'a, TSTypeReference<'a>>) -> bool {
     if let TSTypeName::IdentifierReference(id) = &ts_type_ref.type_name {
